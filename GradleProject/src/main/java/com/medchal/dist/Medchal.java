9fb0c05637ab3bad8ebcc6c06bec4cb0f1a1494e
gitmahender <stoks.alerts@gmail.com>
2019-04-17 20:45:14 -0500

    initial version

---
+++ GradleProject/src/main/java/com/medchal/dist/Medchal.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.medchal.dist;
+
+/**
+ * @author 74
+ *
+ */
+public class Medchal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("This is from the gradle project medchal class");
+
+	}
+
+}
