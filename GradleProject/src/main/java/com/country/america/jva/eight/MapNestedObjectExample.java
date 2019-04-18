824ee60e230b94ce533648c638593ff147b4be1e
gitmahender <stoks.alerts@gmail.com>
2019-04-17 23:28:37 -0500

    added nested object map

---
+++ GradleProject/src/main/java/com/country/america/jva/eight/MapNestedObjectExample.java
@@ -0,0 +1,48 @@
+package com.country.america.jva.eight;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import com.country.america.model.Child;
+import com.country.america.model.Parent;
+
+public class MapNestedObjectExample {
+
+	public static void main(String[] args) {
+		
+		Parent parent1 = new Parent();
+		parent1.setName("Raji");
+		parent1.setAge(60);
+		
+		Child child1 = new Child();
+		child1.setName("Mahender");
+		child1.setAge(30);
+		parent1.setChild(child1);
+		
+		Parent parent2 = new Parent();
+		parent2.setName("Prame");
+		parent2.setAge(80);
+		
+		Child child2 = new Child();
+		child2.setName("Sand");
+		child2.setAge(40);
+		parent2.setChild(child2);
+		
+		
+		//Add to the list
+		List<Parent> parents = Arrays.asList(parent1, parent2);
+		
+		
+		//Create a stream first
+		Stream<Parent> parentStream = parents.stream();
+		
+		//map: loop thru stream, for each parent object get the child and create a new stream of child object
+		Stream<String> children = parentStream.map(Parent::getChild).map(Child::getName);
+		children.forEach(System.out::println);
+		
+		
+
+	}
+
+}
