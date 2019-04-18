df0602cb8dff34c6c658f4339d690187ff754388
gitmahender <stoks.alerts@gmail.com>
2019-04-18 06:15:39 -0500

    ispresent, orelse examples

---
+++ GradleProject/src/main/java/com/country/america/jva/eight/IsPresentExample.java
@@ -0,0 +1,21 @@
+package com.country.america.jva.eight;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class IsPresentExample {
+
+	public static void main(String[] args) {
+
+		List<String> list = Arrays.asList("mahender", "ramesh", "suresh", "mahender", "suresh");
+
+		Optional<String> distinct = list.stream()
+				.filter(x->x.contains("z"))
+				.findFirst();								//statefull intermediate operation
+				
+
+		System.out.println("isPresent: " + distinct.isPresent());
+	}
+
+}
