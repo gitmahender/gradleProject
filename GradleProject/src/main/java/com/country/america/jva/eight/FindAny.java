b7b438822884ee8a5ff73080f6fd3070cfdfc528
gitmahender <stoks.alerts@gmail.com>
2019-04-18 05:54:33 -0500

    findFirst, findAny concept

---
+++ GradleProject/src/main/java/com/country/america/jva/eight/FindAny.java
@@ -0,0 +1,24 @@
+package com.country.america.jva.eight;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class FindAny {
+
+	public static void main(String[] args) {
+
+		List<String> list = Arrays.asList("mahender", "ramesh", "suresh");
+
+		//This returns any first element in its order of execution
+		//This does not return list, but only one element
+		//This is a short-circuit operation because it just needs ‘any’ first element to be returned and terminate the rest of the iteration.
+		Optional<String> optString = list.stream()
+				.filter(x -> x.contains("esh"))			//intermediate operation
+				.findAny();								//terminal operation
+				
+
+		System.out.println("string: " + optString.get());
+	}
+
+}
---
+++ GradleProject/src/main/java/com/country/america/jva/eight/FindFirst.java
@@ -0,0 +1,20 @@
+package com.country.america.jva.eight;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class FindFirst {
+
+	public static void main(String[] args) {
+
+		List<String> list = Arrays.asList("mahender", "ramesh", "suresh");
+
+		//Finds the first one in the list
+		Optional<String> optString = list.stream()
+				.findFirst();					//This is a terminal operation; because it terminates the rest of the execution
+
+		System.out.println("string: " + optString.get());
+	}
+
+}
